d48855ef286cf72182421548d5ae4cbc0148eff8
jianxing04 <1444153948@qq.com>
2025-08-08 11:42:26 +0800

    add a simple gin demonstration

---
+++ SimpleDemo/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"net/http"
+	"github.com/gin-gonic/gin"
+)
+
+func main(){
+	router:=gin.Default()
+	router.GET("/",func(c *gin.Context){
+		c.JSON(http.StatusOK,gin.H{
+			"message":"Welcome to the Gin web server!",
+		})
+	})
+	router.GET("/ping",func(c *gin.Context){
+		c.JSON(http.StatusOK,gin.H{
+			"message":"pong",
+		})
+	})
+	router.GET("/hello/:name",func(c *gin.Context){
+		name:=c.Param("name")
+		c.JSON(http.StatusOK,gin.H{
+			"message":"Hello,"+name+"!",
+		})
+	})
+	router.Run(":8080")
+}
